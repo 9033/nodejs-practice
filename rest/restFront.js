5b9972c268dd6675a452431a0f24ca2b8f75b3ee
9033 <28668509+9033@users.noreply.github.com>
2018-08-19 13:28:53 +0900

    apply promise

--- rest/restFront.js
+++ rest/restFront.js
@@ -1,64 +1,79 @@
-function getUser(){
-    var xhr=new XMLHttpRequest();
-    xhr.onload=function(){
-        if(xhr.status==200){
-            var users=JSON.parse(xhr.responseText);
-            var list=document.getElementById('list');
-            list.innerHTML='';
-            Object.keys(users).map(function (key){
-                var userDiv=document.createElement('div');
-                var span=document.createElement('span');
-                span.textContent=users[key];
-                
-                var edit=document.createElement('button');
-                edit.textContent='수정';
-                edit.addEventListener('click', function(){
-                    var name=prompt('바꿀 이름?');
-                    if(!name){
-                        return alert('이름을 입력안해?');
-                    }
-                    
-                    var xhr=new XMLHttpRequest();
-                    xhr.onload=function(){
-                        if(xhr.status==200){
-                            console.log(xhr.responseText);
-                            getUser();
-                        }else{
-                            console.error(xhr.responseText);
-                        }
-                    };
-                    xhr.open('PUT','/users/'+key);
-                    xhr.setRequestHeader('Content-Type','application/json');
-                    xhr.send(JSON.stringify({name:name}));
-                });
-                
-                var remove=document.createElement('button');
-                remove.textContent='삭제';
-                remove.addEventListener('click', function(){
-                    var xhr=new XMLHttpRequest();
-                    xhr.onload=function(){
-                        if(xhr.status==200){
-                            console.log(xhr.responseText);
-                            getUser();
-                        }else{
-                            console.error(xhr.responseText);
-                        }
-                    };
-                    xhr.open('DELETE','/users/'+key);                    
-                    xhr.send();
-                });
-                
-                userDiv.appendChild(span);
-                userDiv.appendChild(edit);
-                userDiv.appendChild(remove);
-                list.appendChild(userDiv);
-            });
-        }else{
-            console.error(xhr.responseText);
+//mxhr('PUT','/users/'+key,JSON.stringify({name:name}),{'Content-Type':'application/json'});
+function mxhr(method,url,send,header={}){
+    return new Promise((res,rej)=>{
+        let xhr=new XMLHttpRequest();
+        xhr.onload=()=>{
+            if(xhr.status==200||xhr.status==201){
+                res(xhr);
+            }
+            else{
+                rej(xhr);
+            }
+        };
+        xhr.onerror=()=>{rej(xhr);};
+        xhr.open(method,url);        
+        for(let v in header){            
+            xhr.setRequestHeader(v,header[v]);
         }
-    };
-    xhr.open('GET','/users');                    
-    xhr.send();
+        xhr.send(send);
+    });
+}
+
+function getUser(){
+    function useronload(xhr){//refrash user list
+        var users=JSON.parse(xhr.responseText);
+        var list=document.getElementById('list');
+        list.innerHTML='';
+        Object.keys(users).map(function (key){
+            var userDiv=document.createElement('div');
+            var span=document.createElement('span');
+            span.textContent=users[key];
+            
+            var edit=document.createElement('button');
+            edit.textContent='수정';
+            edit.addEventListener('click', function(){
+                var name=prompt('바꿀 이름?');
+                if(!name){
+                    return alert('이름을 입력안해?');
+                }
+                mxhr('PUT','/users/'+key,JSON.stringify({name:name}),{'Content-Type':'application/json'})
+                .then((res)=>{
+                    console.log(res.responseText);
+                    getUser();
+                })
+                .catch((rej)=>{
+                    console.log(rej.responseText);
+                });
+            });
+            
+            var remove=document.createElement('button');
+            remove.textContent='삭제';
+            remove.addEventListener('click', function(){
+                mxhr('DELETE','/users/'+key,'',{})
+                .then((res)=>{
+                    console.log(res.responseText);
+                    getUser();
+                })
+                .catch((rej)=>{
+                    console.log(rej.responseText);
+                });
+            });
+            
+            userDiv.appendChild(span);
+            userDiv.appendChild(edit);
+            userDiv.appendChild(remove);
+            list.appendChild(userDiv);
+        });
+    }
+    
+    mxhr('GET','/users','',{})
+    .then((res)=>{
+        console.log(res.responseText);
+        useronload(res);
+    })
+    .catch((rej)=>{
+        console.error(rej.responseText);
+    });
 }
 window.onload=getUser;
 
@@ -68,17 +83,13 @@ document.getElementById('from').addEventListener('submit',function(e){
     if(!name){
         return alert('이름을 입력안해?');
     }
-    var xhr=new XMLHttpRequest();
-    xhr.onload=function(){
-        if(xhr.status==201){
-            console.log(xhr.responseText);
-            getUser();
-        }else{
-            console.error(xhr.responseText);
-        }
-    };
-    xhr.open('POST','/users');                    
-    xhr.setRequestHeader('Content-Type','application/json');
-    xhr.send(JSON.stringify({name:name}));
+    mxhr('POST','/users',JSON.stringify({name:name}),{'Content-Type':'application/json'})
+    .then((res)=>{
+        console.log(res.responseText);
+        getUser();
+    })
+    .catch((rej)=>{
+        console.log(rej.responseText);
+    });
     e.target.username.value='';
 });
