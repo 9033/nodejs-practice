df9478afca5b9f65c8edae01bd53be75ecee7ada
9033 <28668509+9033@users.noreply.github.com>
2018-08-21 14:29:03 +0900

    express

--- express/learn-express/routes/index.js
+++ express/learn-express/routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var fs=require('fs');
+var myenv;
+try{
+  myenv=fs.readFileSync('9033.env');
+  myenv=JSON.parse(myenv );
+}
+catch(e){
+  myenv={};
+}
+
+router.get('/skyred', function(req, res, next) {
+  res.render('skyred', myenv);
+});
+
 module.exports = router;
